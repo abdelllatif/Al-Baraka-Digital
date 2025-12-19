8029ebd038a45f1545f5fca704d11bff82e2d923
abdelllatif <haissouneabdellatif749@gmail.com>
2025-12-19 16:00:29 +0100

    Adding the method loadUserByUsername to the class UserDetailsServiceImpl

--- src/main/java/com/albaraka/config/CustomUserDetails.java
+++ src/main/java/com/albaraka/config/CustomUserDetails.java
@@ -1,4 +1,48 @@
 package com.albaraka.config;
 
-public class CustomUserDetails {
+
+import com.albaraka.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
 }
--- src/main/java/com/albaraka/config/UserDetailsServiceImpl.java
+++ src/main/java/com/albaraka/config/UserDetailsServiceImpl.java
@@ -1,4 +1,24 @@
 package com.albaraka.config;
 
-public class UserDetailsServiceImpl {
+import com.albaraka.models.User;
+import com.albaraka.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepo;
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepo = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+
+    }
+
 }
