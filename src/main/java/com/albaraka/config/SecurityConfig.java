651608d98e6dacc6c2bb0416b9bc76186dd6b7ad
abdelllatif <haissouneabdellatif749@gmail.com>
2025-12-20 17:13:15 +0100

    adding the security config logic

--- src/main/java/com/albaraka/config/SecurityConfig.java
+++ src/main/java/com/albaraka/config/SecurityConfig.java
@@ -1,18 +1,25 @@
 package com.albaraka.config;
 
+import com.albaraka.enums.Role;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
+    @Autowired
+    JwtAuthenticationFilter jwtFilter;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -22,9 +29,22 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-        
+            .authorizeHttpRequests(auth ->
+            auth.requestMatchers("/auth/**").permitAll()
+                    .requestMatchers("/api/admin/**").hasRole(Role.ADMIN.name())
+                    .requestMatchers("/api/agent/**").hasRole(Role.AGENT_BANCAIRE.name())
+                    .requestMatchers("/api/client/**").hasRole(Role.CLIENT.name())
+                    )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
 
