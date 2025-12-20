51b32c4b1c01faf0f77e356a420a41113d007bbe
abdelllatif <haissouneabdellatif749@gmail.com>
2025-12-20 18:59:41 +0100

    here adding validateOperationOwnership to ResourceAccessValidator

--- src/main/java/com/albaraka/config/ResourceAccessValidator.java
+++ src/main/java/com/albaraka/config/ResourceAccessValidator.java
@@ -1,4 +1,21 @@
-package com.albaraka.config;
+package com.albaraka.security;
 
+import com.albaraka.models.Account;
+import com.albaraka.models.Operation;
+import com.albaraka.models.User;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ResourceAccessValidator {
+
+    public void validateOperationOwnership(Operation operation, Long userId) {
+        if (!operation.getAccountSource().getOwner().getId().equals(userId)) {
+            throw new AccessDeniedException(
+                    "You do not have permission to access operation with id: " + operation.getId()
+            );
+        }
+    }
+
+
 }
