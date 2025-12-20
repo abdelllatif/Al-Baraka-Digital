301d32cb15548085f15e8239a17d46f680dc0434
abdelllatif <haissouneabdellatif749@gmail.com>
2025-12-20 16:57:43 +0100

    adding the JwtAuthenticationFilter

--- src/main/java/com/albaraka/config/JwtAuthenticationFilter.java
+++ src/main/java/com/albaraka/config/JwtAuthenticationFilter.java
@@ -1,4 +1,45 @@
 package com.albaraka.config;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void  doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+             final String autorizationToken = request.getHeader("Authorization");
+             String username = null;
+             String jwt = null;
+             if(autorizationToken !=null && autorizationToken.startsWith("Bearer ")){
+                 jwt = autorizationToken.substring(7);
+                 username= jwtUtil.extractUsername(jwt);
+                 if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                     UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                     if(jwtUtil.validateToken(jwt,userDetails)) {
+                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                         SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()));
+                     }
+                 }
+             }
+        filterChain.doFilter(request, response);
+    }
 }
--- src/main/java/com/albaraka/config/JwtUtil.java
+++ src/main/java/com/albaraka/config/JwtUtil.java
@@ -1,4 +1,38 @@
 package com.albaraka.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+        private final String SECRET_KEY = "mysecret";
+
+        public String extractUsername(String token) {
+            return Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        }
+
+        public Boolean validateToken(String token, UserDetails userDetails) {
+            final String username = extractUsername(token);
+            return (username.equals(userDetails.getUsername()));
+        }
+
+        public String generateToken(UserDetails userDetails) {
+            return Jwts.builder()
+                    .setSubject(userDetails.getUsername())
+                    .setIssuedAt(new Date(System.currentTimeMillis()))
+                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                    .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                    .compact();
+
+    }
 }
