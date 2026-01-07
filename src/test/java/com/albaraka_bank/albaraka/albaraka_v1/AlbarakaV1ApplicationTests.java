63000800ae96e90aaedb9296e7c02e6beac3e623
abdelllatif <haissouneabdellatif749@gmail.com>
2026-01-07 11:14:02 +0100

    Test CI/CD pipeline*

--- src/test/java/com/albaraka_bank/albaraka/albaraka_v1/AlbarakaV1ApplicationTests.java
+++ src/test/java/com/albaraka_bank/albaraka/albaraka_v1/AlbarakaV1ApplicationTests.java
@@ -4,13 +4,10 @@ import com.albaraka.AlbarakaV1Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-        classes = AlbarakaV1Application.class,
-        properties = {
-                "spring.security.oauth2.resourceserver.jwt.issuer-uri=http://127.0.0.1:8080/realms/albaraka-realm"
-        }
-)
+@SpringBootTest(classes = AlbarakaV1Application.class)
 class AlbarakaV1ApplicationTests {
+
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+    }
 }
